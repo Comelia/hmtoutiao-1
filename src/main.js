409d23e7b073725710a2f8a40cf2ef4d865afafc
Comelia <1511867031@qq.com>
2019-07-10 15:55:33 +0800

    使用Element-ui和router

--- src/main.js
+++ src/main.js
@@ -1,8 +1,16 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import Element from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+import router from '@/router'
+
+Vue.use(Element)
 Vue.config.productionTip = false
 
+// 职责1.导入项目需要依赖的包( css/ 包 )
+// 职责2.初始化跟市里 new Vue()
 new Vue({
+  router,
   render: h => h(App)
 }).$mount('#app')
